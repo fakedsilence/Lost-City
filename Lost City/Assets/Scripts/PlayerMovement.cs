b3518e1e323dd209de6b401ac1f4c7c757cbfeac
gaoximing <gaoximing@tuyoogame.com>
2024-10-10 09:22:14 +0800

    新增Ethan角色

--- Lost City/Assets/Scripts/PlayerMovement.cs
+++ Lost City/Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 50f;
 
-    private Rigidbody2D rigidbody2D;
+    private Rigidbody2D rb;
 
     private void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
         float moveH = Input.GetAxis("Horizontal");
         float moveV = Input.GetAxis("Vertical");
-        rigidbody2D.velocity = new Vector2(moveH * speed, moveV * speed);
+        rb.velocity = new Vector2(moveH * speed, moveV * speed);
         if (moveH != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(moveH), 1, 1);
